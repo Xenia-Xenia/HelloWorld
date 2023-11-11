b8fa8bd54918756ca5e3e7b33d04d97584460350
Xenia Zagareva <xeniazagareva@gmail.com>
2023-11-11 23:37:03 +0300

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Ксения Загарьева on 11.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
